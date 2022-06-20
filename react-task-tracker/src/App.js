499a3783f9f291d713768c933bb90b4845f5e5d3
pcarderdev <31299967+pcarderdev@users.noreply.github.com>
2022-06-20 07:17:07 -0400

    Progress

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,34 +1,50 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from 'react'
+import AddTask from "./components/AddTask";
 
 const App = () => {
 
   const [tasks, setTasks] = useState([
-      {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-      },
-    ])
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at school',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    },
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ?
+          { ...task, reminder: !task.reminder } :
+          task))
+  }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <AddTask />
+      {tasks.length > 0 ?
+        (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) :
+        ("Please add a task.")}
     </div>
   )
 }
